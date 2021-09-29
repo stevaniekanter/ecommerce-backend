7d70b275a3a8e8b341f8fa461b9323d742935ed7
Stevanie Kanter <stevaniek@yahoo.com>
2021-09-29 18:07:30 -0400

    updated models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
+  
   {
     sequelize,
     timestamps: false,

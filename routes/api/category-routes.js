5cc4057f111a0d20d0916e249b0c9e0f10a27f15
Stevanie Kanter <stevaniek@yahoo.com>
2021-09-29 22:18:21 -0400

    update catogory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products

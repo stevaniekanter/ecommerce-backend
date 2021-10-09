65818746a2f54644007f1b7f25468cf6b45aec87
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-09 12:44:23 -0400

    updated readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,33 @@
 # Ecommerce - Backend
+
+# Table of Contents
+  * [Description](#description)
+  * [Demonstration](#demonstration)
+  * [Installation](#installation)
+  * [Technology](#technology)
+  * [Screenshot](#screenshot)
+
+# Description
+
+In this assignment, I have created a node app that uses express server and sequalize to sync and work with a database. This app and database can store and update information. 
+
+# Demonstration
+
+* [Video Walkthrough](https://drive.google.com/file/d/1Io3WaGQe_N7AYWATIks6OBJVP6ujO5kT/view?usp=sharing)
+
+* [Github Repository](https://github.com/stevaniekanter/ecommerce-backend)
+
+
+# Installation
+
+npm install
+
+npm run seed 
+
+# Technologies Used
+
+```
+Express
+Node
+mySQL
+```
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Tag, through: ProductTag, as: 'product_tags' }, { model: Category }]
+    });
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag, through: ProductTag, as: 'product_tags' }, { model: Category }]
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -89,8 +112,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productDelete = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productDelete) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
